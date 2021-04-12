8306f23109cdb9ecd0ae22dc43dd8b74d7ea69aa
yanL0752 <80777895+yanL0752@users.noreply.github.com>
2021-04-13 00:12:36 +0200

    adding containers, rows, columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -14,10 +15,53 @@ class Home extends StatelessWidget {
       appBar : AppBar(
         title: Text("Gengar's App"),
         centerTitle: true,
-        backgroundColor: Colors.blueGrey[900],
+        backgroundColor: Colors.grey[900],
       ),
-      body:
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 200.0, vertical: 120.0),
+                margin: EdgeInsets.all(5.0),
+                color: Colors.greenAccent,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 200.0, vertical: 120.0),
+                margin: EdgeInsets.all(5.0),
+                color: Colors.greenAccent,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 200.0, vertical: 120.0),
+                margin: EdgeInsets.all(5.0),
+                color: Colors.greenAccent,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 200.0, vertical: 120.0),
+                margin: EdgeInsets.all(5.0),
+                color: Colors.greenAccent,
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 400.0, vertical: 100.0),
+            margin: EdgeInsets.all(5.0),
+            color: Colors.red[400],
+            ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 400.0, vertical: 100.0),
+            margin: EdgeInsets.all(5.0),
+            color: Colors.red[400],
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 400.0, vertical: 100.0),
+            margin: EdgeInsets.all(5.0),
+            color: Colors.red[400],
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         child: Text("Click"),
         onPressed: () {},
